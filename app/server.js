c59f577eeb4df8504ba705cd7794cf3dc07866a8
John Oakely <jooakley@gmail.com>
2016-02-08 03:16:18 -0500

    Use the heroku port, if provided

--- app/server.js
+++ app/server.js
@@ -2,4 +2,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 var contentDir = __dirname + '/../dist'
-connect().use(serveStatic(contentDir)).listen(8080);
+connect().use(serveStatic(contentDir)).listen(process.env.PORT || 8080);
