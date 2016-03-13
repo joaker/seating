d87149eb2623a38b170d8d04a6b393b47061155c
John Oakely <jooakley@gmail.com>
2016-03-13 18:30:36 -0400

    Stick the sidebar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ import cnames from 'classnames/dedupe';
 import AutoAffix from 'react-overlays/lib/AutoAffix';
 //'nav-sidebar', 'affix',
 const Nav = ({children}) => (
-  <div className={cnames('NavSidebar', styles.nav, 'hidden-xs', 'col-sm-3', 'col-md-2')} data-spy="affix" style={{height:'100%', padding:0}}>
+  <div className={cnames('NavSidebar', styles.nav, 'hidden-xs', 'sticky', 'col-sm-3', 'col-md-2')}>
     <div className={cnames('ScrollingSidebar', cnames.navContent)}>
       <ul className={cnames(styles['links'],'nav', 'nav-pills', 'nav-stacked')} >
         <li key={'main'}><Link to={`/`}>Main</Link></li>
