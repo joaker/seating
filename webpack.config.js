cbeb64abe11e5d92c1413fd82a85f9d049d36cf0
joaker <jooakley@gmail.com>
2018-02-13 17:32:34 -0600

    migrate webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ const fs = require('fs');
 module.exports = {
   devtool: 'source-map',
   cache: true,
-  debug: true,
   // Starting point for the application server
   entry: [
     //'webpack-dev-server/client?http://localhost:8080',
@@ -27,33 +26,32 @@ module.exports = {
       {
         test: /\.jsx?$/, // Apply this loader to all JSX files
         exclude: /node_modules/, // Don't bother with node modules, though
-        loader: 'react-hot!babel', // Hot loading react content and transpile with babel.  Not appropriate for prod
-        cacheable: true
+        loader: 'react-hot-loader!babel-loader', // Hot loading react content and transpile with babel.  Not appropriate for prod
       },
       {
         test: /\.css$/,
         // What is this black magic?
-        loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]',
+        loader: 'style-loader!css-loader?modules&localIdentName=[name]---[local]---[hash:base64:5]',
       },
       {
         test: /\.scss$/,
         //loader: 'style!css!sass'  // NOTE: this works for bundling, but not for css-module use
         loaders: [
-            'style',
-            'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-            'resolve-url',
-            'sass'
+            'style-loader',
+            'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+            'resolve-url-loader',
+            'sass-loader'
         ],
       },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
-        loader: 'file?name=app/generated/[name].[ext]'
+        loader: 'file-loader?name=app/generated/[name].[ext]'
       },
     ]
   },
   // Bundle these files only
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json']
   },
   output: {
     path: __dirname + '/dist', // What folder for the output?
