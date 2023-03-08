7cfa90589287a4b4591110d6f268be4d8a88b693
John Oakley <jooakley@gmail.com>
2023-03-08 15:49:47 -0600

    clean up venue

--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -1,14 +1,12 @@
 import styles from '../style/venue.scss';
 
 import cnames from 'classnames/dedupe';
-import {List, Map} from 'immutable';
+import { List, Map } from 'immutable';
 import React from 'react';
 import { connect } from 'react-redux';
-import {Loader} from 'react-loaders';
 
-import {populateVenue, setVenueGuests, scoreVenue, startOptimization, endOptimization, setMaxDifficulty, toggleVenueDetails, setTemperature} from '../app/action_creators';
+import { populateVenue, setVenueGuests, scoreVenue, startOptimization, endOptimization, setMaxDifficulty, toggleVenueDetails, setTemperature } from '../app/action_creators';
 import * as params from '../data/venue.js';
-import * as scorer from '../app/scorer';
 
 import Layout from './pure/Venue/Layout';
 import Progress from './pure/Progress';
@@ -16,28 +14,13 @@ import optimizer from '../app/optimization/optimizer';
 import createInjectNavigate from './createInjectNavigate';
 
 
-const layoutDimensions = {rowCount: params.rowCount, columnCount: params.tablesPerRow};
-
-
-
 const marks = {};
 
-
-for(let i = params.minSize; i <= params.maxSize; i+= params.interval){
+for (let i = params.minSize; i <= params.maxSize; i += params.interval) {
   const value = i;
   const message = (value == params.minSize) ? 'Quick' : (value == params.maxSize ? 'Thorough' : '');
   marks[value] = message;
 }
-
-// range(maxSize-minSize+1).map(index => {
-//   const value = minSize + index;
-//   const message = (value == minSize) ? 'Quick' : (value == maxSize ? 'Thorough' : '');
-//   marks[value] = message;
-// });
-
-console.log('marks!')
-console.log('marks!')
-console.log('marks!')
 
 class UnconnectedVenue extends React.Component {
   constructor(props, ctx) {
@@ -54,38 +37,38 @@ class UnconnectedVenue extends React.Component {
     this.getScoreType = this.getScoreType.bind(this);
   }
 
-  componentWillMount(){
+  componentWillMount() {
 
     const guests = this.props.guests;
     const numberOfGuests = guests.length;
 
-    if(numberOfGuests) return;
+    if (numberOfGuests) return;
 
     this.props.navigate('/generate-guests');
 
   }
 
-  handleChange(event){
+  handleChange(event) {
     const state = this.state || {};
     this.setState({ newGuest: event.target.value });
   }
 
-  getRawScore(){
+  getRawScore() {
     return this.props.score;
   }
 
 
-  getFriendlyScore(){
+  getFriendlyScore() {
     const friendly = params.maxScore + this.getRawScore();
     return friendly;
   }
 
   getScoreType() {
-    if(!this.hasGuests()) return '';
+    if (!this.hasGuests()) return '';
     const s = this.getFriendlyScore();
-    if(s >= params.maxScore) return "perfect";
-    if( s >= 90) return "good";
-    if( s >= 80) return "ok";
+    if (s >= params.maxScore) return "perfect";
+    if (s >= 90) return "good";
+    if (s >= 80) return "ok";
     return "bad";
   }
 
@@ -93,70 +76,25 @@ class UnconnectedVenue extends React.Component {
     return this.props.guests && this.props.guests.length;
   }
 
-  render(){
-
-    const hasGuests = this.props.guests && this.props.guests.length;
-    const noGuests = !hasGuests;
-
-    const optimizeTip = hasGuests ? 'Search for a better arrangement' : 'Populate the venue to allow searching';
-    const populateTip = hasGuests ? 'Clear and make new guests with new seat assignments' : 'Randomly fill the venue with new guests';
-
-    const clearTableStyle = {
-      display: 'inline-block',
-      marginLeft: '.5em',
-      float: 'right',
-    };
-
-    const ibStyle = {display: 'inline-block'};
-    const scoreType = this.getScoreType();
-    const scoreStyle = styles[scoreType];
-    const scoring = '' ;
-    //  this.props.hasScore ?
-    //   (<div className={cnames(styles.scoring, "Score", scoreStyle)} style={ibStyle}>
-    //     <div className={cnames(styles.title, "title")} style={ibStyle}>Scoring</div>
-    //     <div className={cnames(styles.value, "scoring")} style={ibStyle}>{this.getFriendlyScore()}</div>
-    //   </div>):
-    //   ('');
-
-    const optimizationIndicator = (
-      <div className={cnames(styles.busy)}>
-        Optimizing
-        <Loader type='ball-grid-beat' active={true}/>
-      </div>
-    );
-
-
-    const spt = this.props.seatsPerTable;
-
+  render() {
     return (
       <div className={cnames(styles.venue, "Venue")}>
         <div className={cnames('headerTable', 'container-fluid')}>
           <div className={cnames('row')}>
             <div className={cnames('col-xs-12')}>
-              <h2 style={{display: 'block'}}>
-                Venue {this.props.lastRunTime ? (<div style={{display:'inline-block'}}><h4 className="text-muted text-veryMuted" style={{display: 'inline-block'}}>Last Run: {this.props.lastRunTime}</h4></div>) : ''}
-                <Progress ratio={this.props.progressRatio}/>
+              <h2 style={{ display: 'block' }}>
+                Venue {this.props.lastRunTime ? (<div style={{ display: 'inline-block' }}><h4 className="text-muted text-veryMuted" style={{ display: 'inline-block' }}>Last Run: {this.props.lastRunTime}</h4></div>) : ''}
+                <Progress ratio={this.props.progressRatio} />
               </h2>
             </div>
           </div>
         </div>
-        <Layout guestCount={this.props.guestCount} seatsPerTable={this.props.seatsPerTable}/>
+        <Layout guestCount={this.props.guestCount} seatsPerTable={this.props.seatsPerTable} />
       </div>
     );
   }
 }
 
-// UnconnectedVenue.contextTypes = {
-//   router: React.PropTypes.object.isRequired
-// }
-
-const calculateVenueScore = (guests, tableSize) => {
-  let score = 0;
-  for(let i = 0; i < guests.length; i += tableSize){
-    score += scorer.scoreTable(guests.slice(i, i+tableSize));
-  }
-  return score;
-}
 const opimizationDispatchRelay = (dispatch) => ({
   start: () => dispatch(startOptimization()),
   update: (list, ratio) => dispatch(setVenueGuests(list, ratio)),
@@ -191,8 +129,8 @@ const mapStateToProps = (state = Map()) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  populate: () => {dispatch(populateVenue()); dispatch(scoreVenue());},
-  optimizeGuests: (guests,temperature = params.defaultTemperature, mode=params.defaultMode, score) => optimizer.run(makeScoredList(guests, score), opimizationDispatchRelay(dispatch), temperature, mode),
+  populate: () => { dispatch(populateVenue()); dispatch(scoreVenue()); },
+  optimizeGuests: (guests, temperature = params.defaultTemperature, mode = params.defaultMode, score) => optimizer.run(makeScoredList(guests, score), opimizationDispatchRelay(dispatch), temperature, mode),
   scoreTables: () => dispatch(scoreVenue()),
   setDifficulty: (difficulty) => dispatch(setMaxDifficulty(difficulty)),
   toggleVenueDetails: () => dispatch(toggleVenueDetails()),
