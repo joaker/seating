ab01379e3150cd32989a7109616c9ac143a7d8e8
John Oakley <jooakley@gmail.com>
2023-03-08 15:29:37 -0600

    get this heap running again

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
 
-// var nodeModules = {};
-// fs.readdirSync('node_modules')
-//   .filter(function(x) {
-//     return ['.bin'].indexOf(x) === -1;
-//   })
-//   .forEach(function(mod) {
-//     nodeModules[mod] = 'commonjs ' + mod;
-//   });
-
 module.exports = {
   devtool: 'source-map',
   cache: true,
